21796d29459643453bf12f94693bfeceed120277
Michael Ansbro <mansbro@strozfriedberg.com>
2017-08-09 18:28:50 +0800

    Updated lists & strings PS

--- Lists & Strings/PowerShell.ps1
+++ Lists & Strings/PowerShell.ps1
@@ -1,13 +1,53 @@
 # Lists, Strings
-
 # If your language of choice doesn’t have a build in list and/or string type (e.g. you use C), these exercises should also be solvable for arrays. However, some solutions are very different between an array-based list (like C++’s vector) and a pointer based list (like C++’s list), at least if you care about the efficiency of your code. So you might want to either find a library, or investigate how to implement your own linked list if your language doesn’t have it.
 
 # Write a function that returns the largest element in a list.
+    function largestElement {
+        param (
+            [string[]]$items
+        )
+        $n = 0
+        foreach ($fruit in $fruits) {
+            if ($fruit.length -gt $n) {
+                $n = $fruit.length
+                $longest = $fruit
+            }  
+        }
+        write-output "The longest element in the array is $longest"
+    }
 # Write function that reverses a list, preferably in place.
+    function reverseList {
+        param (
+            $items
+        )
+        $r = @()
+        $i = $items.count
+        foreach ($item in $items) {
+            $r += $items[$i-1]
+            $i--
+        }
+        write-output $r
+    }
 # Write a function that checks whether an element occurs in a list.
+
 # Write a function that returns the elements on odd positions in a list.
+  function oddItems {
+    param (
+        $items
+    )
+    $odds = @()
+    foreach ($n in (1..($fruits.count))){
+        if ($n % 2 -eq 1) {
+            $odds += $n
+        } 
+    }
+    foreach ($odd in $odds) {
+        write-output $fruits[$odd]
+    }
+}
 # Write a function that computes the running total of a list.
 # Write a function that tests whether a string is a palindrome.
+    
 # Write three functions that compute the sum of the numbers in a list: using a for-loop, a while-loop and recursion. (Subject to availability of these constructs in your language of choice.)
 # Write a function on_all that applies a function to every element of a list. Use it to print the first twenty perfect squares (a natural number n is a perfect square if it can be written as n=m*m for some other natural number m. 1,4,9,16,25 are the first 5).
 # Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
