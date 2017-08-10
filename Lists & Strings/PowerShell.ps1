6f3a6b4861603df4b444cc6d3bec83767ef3b8c8
Michael Ansbro <mansbro@strozfriedberg.com>
2017-08-10 18:12:06 +0800

    Updated

--- Lists & Strings/PowerShell.ps1
+++ Lists & Strings/PowerShell.ps1
@@ -47,11 +47,43 @@
 }
 # Write a function that computes the running total of a list.
 # Write a function that tests whether a string is a palindrome.
-    
+    function isThisStringAPalindrome ([string]$string){
+        $text = $string -replace ' ', ''
+        $array = $text.ToCharArray()
+        [array]::Reverse($array)
+        $text -like (-join $array) 
+    }
 # Write three functions that compute the sum of the numbers in a list: using a for-loop, a while-loop and recursion. (Subject to availability of these constructs in your language of choice.)
+    function sumFor ($numbers) {
+        $sum = 0
+        for ($i = 0; $i -lt $numbers.Count; $i++) {
+            $sum += $numbers[$i]
+        }
+        $sum
+    }
+    function sumWhile ($numbers) {
+        $i = 0
+        while ($i -lt $numbers.count) {
+            $sum += $numbers[$i]
+            $i++
+        }
+        $sum
+    }
+    function sumRecurse ($numbers) {
+        if ($numbers.count -eq 0) {
+            write-output 0
+        } else {
+            return $numbers[0] + (sumRecurse $numbers[1..$numbers.length])
+        }
+    }
+
 # Write a function on_all that applies a function to every element of a list. Use it to print the first twenty perfect squares (a natural number n is a perfect square if it can be written as n=m*m for some other natural number m. 1,4,9,16,25 are the first 5).
 # Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
+    function concatLists($a,$b) {
+        return [string]$c = $a + $b
+    }
 # Write a function that combines two lists by alternatingly taking elements, e.g. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+    function 
 # Write a function that merges two sorted lists into a new sorted list. [1,4,6],[2,3,5] → [1,2,3,4,5,6]. You can do this quicker than concatenating them followed by a sort.
 # Write a function that rotates a list by k elements. For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]. Try solving this without creating a copy of the list. How many swap or move operations do you need?
 # Write a function that computes the list of the first 100 Fibonacci numbers.
