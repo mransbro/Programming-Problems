0785c1cd92387936ec8183623574b58ecf11face
System Administrator <root@Michaels-MacBook-Pro.local>
2017-08-13 12:41:45 +0800

    Updated file

--- Lists & Strings/PowerShell.ps1
+++ Lists & Strings/PowerShell.ps1
@@ -3,6 +3,8 @@
 # If your language of choice doesn’t have a build in list and/or string type (e.g. you use C), these exercises should also be solvable for arrays. However, some solutions are very different between an array-based list (like C++’s vector) and a pointer based list (like C++’s list), at least if you care about the efficiency of your code. So you might want to either find a library, or investigate how to implement your own linked list if your language doesn’t have it.
 
 # Write a function that returns the largest element in a list.
+function longestItem ($items) {$items | Sort-Object -Property  | select -first 1}
+
 # Write function that reverses a list, preferably in place.
 # Write a function that checks whether an element occurs in a list.
 # Write a function that returns the elements on odd positions in a list.
